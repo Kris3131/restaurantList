7db4e4f6e11d018d0aa8db22051233739cfd692f
Kris <krischiang0301@gmail.com>
2022-08-01 15:23:40 +0800

    feat: modify mongoose version

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on('error', () => console.log('connect error'));
 db.once('open', () => console.log('connect success'));
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,14 +1,4 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', () => console.log('connect error'));
-db.once('open', () => console.log('connect success'));
 
 const Schema = mongoose.Schema;
 const restaurantSchema = new Schema({
