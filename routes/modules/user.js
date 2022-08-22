01c07d1dd9893029ba6dcddcc3f85b25193ae5d8
Kris <krischiang0301@gmail.com>
2022-08-22 21:36:23 +0800

    feat: Add logout feature

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -37,5 +37,9 @@ router.post('/register', (req, res) => {
 		})
 		.catch((err) => console.log(err))
 })
+router.get('/logout', (req, res) => {
+	req.logout()
+	res.redirect('/user/login')
+})
 
 module.exports = router
