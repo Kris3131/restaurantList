40eeeaa17698ef85983af0f106587f30cbb60204
Kris <krischiang0301@gmail.com>
2022-08-22 14:51:12 +0800

    feat: Add login / register routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
+const user = require('./modules/user')
 
 const router = express.Router()
 
 router.use('/', home)
+router.use('/user', user)
 router.use('/restaurants', restaurant)
 
 module.exports = router
