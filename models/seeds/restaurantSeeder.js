62b1815495bf97621a78d1dcea1bb546ba485e93
Kris <krischiang0301@gmail.com>
2022-08-25 13:23:29 +0800

    fix: restaurantSeed data

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,32 +1,25 @@
-const Restaurant = require('../Restaurant')
-const restaurantList = require('../../restaurant.json').results
-const User = require('../User')
-const userList = require('../../user.json').users
-const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose')
+const bcrypt = require('bcryptjs')
+const Restaurant = require('../Restaurant')
+const User = require('../User')
+const restaurantList = require('./restaurant.json').restaurants
+const userList = require('./user.json').users
 
 db.once('open', () => {
-	Promise.all(
+	return Promise.all(
 		userList.map((user) => {
-			const { name, email, password } = user
+			const { name, email, password, restaurantIndex } = user
 			return User.create({
 				name,
 				email,
 				password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
 			}).then((user) => {
-				if (user.name === 'user1') {
-					for (let i = 0; i < 3; i++) {
-						restaurantList[i].userId = user._id
-					}
-				}
-				if (user.name === 'user2') {
-					for (let i = 3; i < 6; i++) {
-						restaurantList[i].userId = user._id
-					}
-				}
-				return Restaurant.create(restaurantList)
-					.then(() => console.log(`restaurant data done`))
-					.catch((err) => console.log(err))
+				const restaurantSeeder = restaurantIndex.map((index) => {
+					const restaurant = restaurantList[index]
+					restaurant.userId = user._id
+					return restaurant
+				})
+				return Restaurant.create(restaurantSeeder)
 			})
 		})
 	)
@@ -34,5 +27,5 @@ db.once('open', () => {
 			console.log(`User and Restaurant seed data had been created`)
 			process.exit()
 		})
-		.catch((error) => console.log('error'))
+		.catch((error) => console.log(error))
 })
